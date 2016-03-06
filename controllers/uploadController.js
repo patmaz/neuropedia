67106dfb3cbb9b3b8b1fe0ad97b4f8ae9d1390c5
patmaz <patrykiewicz@gmail.com>
2016-03-06 10:53:53 +0100

    upload files filter and limits

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -2,19 +2,30 @@ var fs = require("fs");
 var multer = require('multer');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads')
+        cb(null, './public/uploads')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname);
     }
 });
-var upload = multer({ storage: storage });
+var upload = multer({
+    storage: storage, 
+    limits: { fileSize: 30 * 1000 },
+    fileFilter: function(req, file, cb) {
+        if (file.mimetype === "image/jpeg") {
+            cb(null, true);
+        } else {
+            return cb(new Error("only jpegs!"));
+        }
+    }
+});
 
 module.exports = function (app) {
 
-//    app.post('/upload', upload.single('somefile'), function (req, res) {
-//
-//        console.log(req.file);
-//
-//    });
+    app.post('/upload', upload.single('somefile'), function (req, res) {
+
+        console.log(req.file);
+        res.redirect("/#/upload");
+        
+    });
 }
